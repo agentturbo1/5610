85e7d58190f254732723bd55fee214be86ae7db3
Lukas Gust Desktop <lukasgust31@gmail.com>
2018-11-10 11:37:57 -0700

    Reformatted code, fixed runtime warnings

--- reciever.py
+++ reciever.py
@@ -7,6 +7,7 @@ import math
 c = 299792458  # speed of light
 sid_day = 86164.0899999  # sidereal day in secs
 
+
 def rad2dms(rad):
     '''
     Converts decimal radians to degrees, minutes, seconds
@@ -59,132 +60,119 @@ for hh in range(num_signals):
 
 all_sat_data = np.array(all_sat_data)
 
-#******************#
+# ******************#
 num_time_steps = all_sat_data.shape[0]
 write_to_output = []
 for y in range(num_time_steps):
-    sat_data=np.squeeze(all_sat_data[y])
+    sat_data = np.squeeze(all_sat_data[y])
 
-    num_sats=len(sat_data[:,0]);#the number of satellites above the horizon
-    #I want to take 4 of the satellites we can see from random, and use their data to compute vehicle position
-    test_indices=random.sample(range(num_sats),4)
-    four_sat_data=[];
-    four_sat_data.append(sat_data[test_indices[0],:])
-    four_sat_data.append(sat_data[test_indices[1],:])
-    four_sat_data.append(sat_data[test_indices[2],:])
-    four_sat_data.append(sat_data[test_indices[3],:])
+    num_sats = sat_data[:, 0].shape[0]  # the number of satellites above the horizon
+    # I want to take 4 of the satellites we can see from random, and use their data to compute vehicle position
+    test_indices = random.sample(range(num_sats), 4)
+    four_sat_data = [sat_data[test_indices[0], :], sat_data[test_indices[1], :], sat_data[test_indices[2], :],
+                     sat_data[test_indices[3], :]];
 
-    #now we have the satellite data for that step, let's form the F matrix#
-    sat1_pos=four_sat_data[0][2:5]
-    sat2_pos=four_sat_data[1][2:5]
-    sat3_pos=four_sat_data[2][2:5]
-    sat4_pos=four_sat_data[3][2:5]
-    t_sat1=four_sat_data[0][1]
-    t_sat2=four_sat_data[1][1]
-    t_sat3=four_sat_data[2][1]
-    t_sat4=four_sat_data[3][1]
+    # now we have the satellite data for that step, let's form the F matrix#
+    sat1_pos = four_sat_data[0][2:5]
+    sat2_pos = four_sat_data[1][2:5]
+    sat3_pos = four_sat_data[2][2:5]
+    sat4_pos = four_sat_data[3][2:5]
+    t_sat1 = four_sat_data[0][1]
+    t_sat2 = four_sat_data[1][1]
+    t_sat3 = four_sat_data[2][1]
+    t_sat4 = four_sat_data[3][1]
 
-    #get the initial location of the car at time t, to start the iteration of Newton's method#
-    init_b12_loc=[-1795125.28,-4477274.36,4158393.45];#this is the location of b12
-    rot_vec=[[np.cos(2*np.pi*t_sat1/(86164.09)),-np.sin(2*np.pi*t_sat1/(86164.09)),0],[np.sin(2*np.pi*t_sat1/(86164.09)),np.cos(2*np.pi*t_sat1/(86164.09)),0],[0,0,1]];
-    car_init_loc=np.matmul(rot_vec,init_b12_loc)
+    # get the initial location of the car at time t, to start the iteration of Newton's method#
+    init_b12_loc = [-1795125.28, -4477274.36, 4158393.45];  # this is the location of b12
+    rot_vec = [[np.cos(2 * np.pi * t_sat1 / (86164.09)), -np.sin(2 * np.pi * t_sat1 / (86164.09)), 0],
+               [np.sin(2 * np.pi * t_sat1 / (86164.09)), np.cos(2 * np.pi * t_sat1 / (86164.09)), 0], [0, 0, 1]];
+    car_init_loc = np.matmul(rot_vec, init_b12_loc)
 
-    step_num=0
-    #this is the application of Newton's method where the jacobian is formed, and then updated#
-    while step_num<100:
+    step_num = 0
+    # this is the application of Newton's method where the jacobian is formed, and then updated#
+    while step_num < 100:
+        Fs1 = np.linalg.norm(sat2_pos - car_init_loc) - np.linalg.norm(sat1_pos - car_init_loc) - c * (t_sat1 - t_sat2);
+        Fs2 = np.linalg.norm(sat3_pos - car_init_loc) - np.linalg.norm(sat2_pos - car_init_loc) - c * (t_sat2 - t_sat3);
+        Fs3 = np.linalg.norm(sat4_pos - car_init_loc) - np.linalg.norm(sat3_pos - car_init_loc) - c * (t_sat3 - t_sat4);
+        F_mat = [Fs1, Fs2, Fs3]
+        # Now form the Jacobian matrix#
+        J11 = ((sat1_pos[0] - car_init_loc[0]) / np.linalg.norm(sat1_pos - car_init_loc)) - (
+                (sat2_pos[0] - car_init_loc[0]) / np.linalg.norm(sat2_pos - car_init_loc))
+        J12 = ((sat1_pos[1] - car_init_loc[1]) / np.linalg.norm(sat1_pos - car_init_loc)) - (
+                (sat2_pos[1] - car_init_loc[1]) / np.linalg.norm(sat2_pos - car_init_loc))
+        J13 = ((sat1_pos[2] - car_init_loc[2]) / np.linalg.norm(sat1_pos - car_init_loc)) - (
+                (sat2_pos[2] - car_init_loc[2]) / np.linalg.norm(sat2_pos - car_init_loc))
+        J21 = ((sat2_pos[0] - car_init_loc[0]) / np.linalg.norm(sat2_pos - car_init_loc)) - (
+                (sat3_pos[0] - car_init_loc[0]) / np.linalg.norm(sat3_pos - car_init_loc))
+        J22 = ((sat2_pos[1] - car_init_loc[1]) / np.linalg.norm(sat2_pos - car_init_loc)) - (
+                (sat3_pos[1] - car_init_loc[1]) / np.linalg.norm(sat3_pos - car_init_loc))
+        J23 = ((sat2_pos[2] - car_init_loc[2]) / np.linalg.norm(sat2_pos - car_init_loc)) - (
+                (sat3_pos[2] - car_init_loc[2]) / np.linalg.norm(sat3_pos - car_init_loc))
+        J31 = ((sat3_pos[0] - car_init_loc[0]) / np.linalg.norm(sat3_pos - car_init_loc)) - (
+                (sat4_pos[0] - car_init_loc[0]) / np.linalg.norm(sat4_pos - car_init_loc))
+        J32 = ((sat3_pos[1] - car_init_loc[1]) / np.linalg.norm(sat3_pos - car_init_loc)) - (
+                (sat4_pos[1] - car_init_loc[1]) / np.linalg.norm(sat4_pos - car_init_loc))
+        J33 = ((sat3_pos[2] - car_init_loc[2]) / np.linalg.norm(sat3_pos - car_init_loc)) - (
+                (sat4_pos[2] - car_init_loc[2]) / np.linalg.norm(sat4_pos - car_init_loc))
+        J_Mat = [[J11, J12, J13], [J21, J22, J23], [J31, J32, J33]]
+        Jinv = np.linalg.inv(J_Mat)
+        diff_term = np.matmul(Jinv, F_mat)
+        new_k = car_init_loc - diff_term
+        step_num = step_num + 1;
+        car_init_loc = new_k
 
+        # now, we want to convert the location to degree minute and second
+        R = 6.367444500000000000 * 10 ** 6;
 
-        Fs1=np.linalg.norm(sat2_pos-car_init_loc)-np.linalg.norm(sat1_pos-car_init_loc)-c*(t_sat1-t_sat2);
-        Fs2=np.linalg.norm(sat3_pos-car_init_loc)-np.linalg.norm(sat2_pos-car_init_loc)-c*(t_sat2-t_sat3);
-        Fs3=np.linalg.norm(sat4_pos-car_init_loc)-np.linalg.norm(sat3_pos-car_init_loc)-c*(t_sat3-t_sat4);
-        F_mat=[Fs1,Fs2,Fs3]
-        #Now form the Jacobian matrix#
-        J11=((sat1_pos[0]-car_init_loc[0])/np.linalg.norm(sat1_pos-car_init_loc))-((sat2_pos[0]-car_init_loc[0])/np.linalg.norm(sat2_pos-car_init_loc))
-        J12=((sat1_pos[1]-car_init_loc[1])/np.linalg.norm(sat1_pos-car_init_loc))-((sat2_pos[1]-car_init_loc[1])/np.linalg.norm(sat2_pos-car_init_loc))
-        J13=((sat1_pos[2]-car_init_loc[2])/np.linalg.norm(sat1_pos-car_init_loc))-((sat2_pos[2]-car_init_loc[2])/np.linalg.norm(sat2_pos-car_init_loc))
-        J21=((sat2_pos[0]-car_init_loc[0])/np.linalg.norm(sat2_pos-car_init_loc))-((sat3_pos[0]-car_init_loc[0])/np.linalg.norm(sat3_pos-car_init_loc))
-        J22=((sat2_pos[1]-car_init_loc[1])/np.linalg.norm(sat2_pos-car_init_loc))-((sat3_pos[1]-car_init_loc[1])/np.linalg.norm(sat3_pos-car_init_loc))
-        J23=((sat2_pos[2]-car_init_loc[2])/np.linalg.norm(sat2_pos-car_init_loc))-((sat3_pos[2]-car_init_loc[2])/np.linalg.norm(sat3_pos-car_init_loc))
-        J31=((sat3_pos[0]-car_init_loc[0])/np.linalg.norm(sat3_pos-car_init_loc))-((sat4_pos[0]-car_init_loc[0])/np.linalg.norm(sat4_pos-car_init_loc))
-        J32=((sat3_pos[1]-car_init_loc[1])/np.linalg.norm(sat3_pos-car_init_loc))-((sat4_pos[1]-car_init_loc[1])/np.linalg.norm(sat4_pos-car_init_loc))
-        J33=((sat3_pos[2]-car_init_loc[2])/np.linalg.norm(sat3_pos-car_init_loc))-((sat4_pos[2]-car_init_loc[2])/np.linalg.norm(sat4_pos-car_init_loc))
-        J_Mat=[[J11,J12,J13],[J21,J22,J23],[J31,J32,J33]]
-        Jinv=np.linalg.inv(J_Mat)
-        diff_term=np.matmul(Jinv,F_mat)
-        new_k=car_init_loc-diff_term
-        step_num=step_num+1;
-        car_init_loc=new_k
+    h = np.sqrt(car_init_loc[0] ** 2 + car_init_loc[1] ** 2 + car_init_loc[2] ** 2) - R
+    x = car_init_loc[0]
+    y = car_init_loc[1]
+    z = car_init_loc[2]
 
-     #now, we want to convert the location to degree minute and second
-        R=6.367444500000000000*10**6;
+    # HERE IS WHERE WE CONVERT THE CARTESIAN COORDINATES TO OUTPUT FORM#
+    # now we get psi#
+    if x ** 2 + y ** 2 != 0:
+        psi = np.arctan(z / np.sqrt(x ** 2 + y ** 2))
+    if x == 0 and y == 0 and z > 0:
+        psi = np.pi / 2
+    if x == 0 and y == 0 and z < 0:
+        psi = -1 * np.pi / 2
+    NS = 1;
+    if psi < 0:
+        NS = -1
+        psi = np.abs(psi)
 
+    # now we get lambda#
+    if x > 0 and y > 0:
+        lam = np.arctan(y / x)
+    if x < 0 and y > 0:
+        lam = np.pi + np.arctan(y / x)
+    if x > 0 and y < 0:
+        lam = 2 * np.pi + np.arctan(y / x)
 
-    h=np.sqrt(car_init_loc[0]**2+car_init_loc[1]**2+car_init_loc[2]**2)-R
-    x=car_init_loc[0]
-    y=car_init_loc[1]
-    z=car_init_loc[2]
+    # lam_dms=dec2dms(lam)
+    psi_dms = rad2dms(psi)
 
+    # Now we have lambda at t=0, but... we're not at t=0, so we need to get that#
 
+    # what is the time at the car?#
 
+    car_time = ((np.linalg.norm(sat2_pos - car_init_loc)) / c) + t_sat2;
+    extra_rot = (car_time / sid_day) * np.pi * 2
+    adj_lam = lam - extra_rot
+    # now we have lam make sure it is between 0 and pi#
+    if lam > 0 and lam < np.pi:
+        EW = 1
+    if lam < 0 and np.abs(lam) < np.pi:
+        EW = -1
+        lam = np.abs(lam)
+    if lam < 0 and np.abs(lam) > np.pi:
+        EW = 1
+        lam = 2 * np.pi - np.abs(lam)
+    if lam > np.pi:
+        EW = -1;
+        lam = 2 * np.pi - lam
+    lam_dms = rad2dms(np.abs(adj_lam))
 
-
-
-    #HERE IS WHERE WE CONVERT THE CARTESIAN COORDINATES TO OUTPUT FORM#
-    #now we get psi#
-    if x**2+y**2!=0:
-        psi=np.arctan(z/np.sqrt(x**2+y**2))
-    if x==0 and y==0 and z>0:
-        psi=np.pi/2
-    if x==0 and y==0 and z<0:
-            psi=-1*np.pi/2
-    NS=1;
-    if psi<0:
-        NS=-1
-        psi=np.abs(psi)
-
-
-
-
-
-    #now we get lambda#
-    if x>0 and y>0:
-        lam=np.arctan(y/x)
-    if x<0 and y>0:
-        lam=np.pi+np.arctan(y/x)
-    if x>0 and y<0:
-        lam=2*np.pi+np.arctan(y/x)
-
-
-
-
-    #lam_dms=dec2dms(lam)
-    psi_dms=rad2dms(psi)
-
-
-    #Now we have lambda at t=0, but... we're not at t=0, so we need to get that#
-
-    #what is the time at the car?#
-
-
-
-
-    car_time=((np.linalg.norm(sat2_pos-car_init_loc))/c)+t_sat2;
-    extra_rot=(car_time/sid_day)*np.pi*2
-    adj_lam=lam-extra_rot
-    #now we have lam make sure it is between 0 and pi#
-    if lam>0 and lam<np.pi:
-        EW=1
-    if lam <0 and np.abs(lam)<np.pi:
-        EW=-1
-        lam=np.abs(lam)
-    if lam<0 and np.abs(lam)>np.pi:
-        EW=1
-        lam=2*np.pi-np.abs(lam)
-    if lam>np.pi:
-        EW=-1;
-        lam=2*np.pi-lam
-    lam_dms=rad2dms(np.abs(adj_lam))
-
-
-    to_be_output=[car_time,psi_dms[0],psi_dms[1],psi_dms[2],NS,lam_dms[0],lam_dms[1],lam_dms[2],EW,h]
+    to_be_output = [car_time, psi_dms[0], psi_dms[1], psi_dms[2], NS, lam_dms[0], lam_dms[1], lam_dms[2], EW, h]
     write_to_output.append(to_be_output)
--- satellite.py
+++ satellite.py
@@ -1,10 +1,11 @@
-#now, get all the data from the .dat file
-#Satellite Program#
-#****Compute the Cartesian Location of the Vehicle****#
-#get the data from a particular line#
+# now, get all the data from the .dat file
+# Satellite Program#
+# ****Compute the Cartesian Location of the Vehicle****#
+# get the data from a particular line#
 import numpy as np
 import sys
 import math
+
 
 # Now convert them to decimal
 def dms2rad(degrees, minutes, seconds):
@@ -18,11 +19,13 @@ def dms2rad(degrees, minutes, seconds):
     :type seconds number
     :return: number
     '''
-    rad = math.pi * (degrees + minutes / 60 + seconds / 3600) / 180 # just convert back to decimal degrees then to radians
-    return(rad)
+    rad = math.pi * (
+                degrees + minutes / 60 + seconds / 3600) / 180  # just convert back to decimal degrees then to radians
+    return (rad)
 
 
-def sat_pos(u, v, p, t, Radius, h_s, theta_s, dat_pi): #T his function gives the postion of the satlelline porided the given parameters
+def sat_pos(u, v, p, t, Radius, h_s, theta_s,
+            dat_pi):  # T his function gives the postion of the satlelline porided the given parameters
     '''
     Computes geographical position of satelite
     :param u:
@@ -36,7 +39,7 @@ def sat_pos(u, v, p, t, Radius, h_s, theta_s, dat_pi): #T his function gives the
     :return:
     '''
     x_s = (Radius + h_s) * (u * np.cos((2 * dat_pi * t) / p + theta_s) + v * np.sin(2 * dat_pi * t / p + theta_s))
-    return(x_s)
+    return (x_s)
 
 
 vehicle_input_str = sys.stdin.read().rstrip()
@@ -49,7 +52,7 @@ for i, line in enumerate(pipe_input):
     if path_point.shape[0] == 10:
         path_data[i] = np.array(path_point, dtype='float')
 
-to_be_output = [];#this will have the data from the satellites who are above the horizon appended at each step
+to_be_output = [];  # this will have the data from the satellites who are above the horizon appended at each step
 linear_sat_dat = np.zeros((24 * 9))
 
 # height and Radius
@@ -73,67 +76,72 @@ with open('./data.dat', 'r') as initial_loc:
         line_index += 1
 
 for ns in range(num_steps):
-    prelim_path_data=path_data[ns,:];
-    #latitude angles
-    deg_lat=prelim_path_data[1]
-    min_lat=prelim_path_data[2]
-    sec_lat= prelim_path_data[3]
+    prelim_path_data = path_data[ns, :]
+    # latitude angles
+    deg_lat = prelim_path_data[1]
+    min_lat = prelim_path_data[2]
+    sec_lat = prelim_path_data[3]
 
-     #longitude angles
-    deg_lon=prelim_path_data[5]
-    min_lon=prelim_path_data[6]
-    sec_lon= prelim_path_data[7]
+    # longitude angles
+    deg_lon = prelim_path_data[5]
+    min_lon = prelim_path_data[6]
+    sec_lon = prelim_path_data[7]
 
     init_sat_dat = np.reshape(linear_sat_dat, (24, 9))
     # R=6.367444500000000000*10**6;
-    h_v=prelim_path_data[9]
+    h_v = prelim_path_data[9]
 
-    psi_dec=dms2rad(deg_lat,min_lat,sec_lat)*prelim_path_data[4];
-    lam_dec=dms2rad(deg_lon,min_lon,sec_lon)*prelim_path_data[8];
-    t_v=prelim_path_data[0]
+    psi_dec = dms2rad(deg_lat, min_lat, sec_lat) * prelim_path_data[4]
+    lam_dec = dms2rad(deg_lon, min_lon, sec_lon) * prelim_path_data[8]
+    t_v = prelim_path_data[0]
 
-    #Now convert these to cartesian coordinates#
-    x_pos=(R+h_v)*(np.cos(psi_dec)*np.cos(lam_dec));
-    y_pos=(R+h_v)*(np.cos(psi_dec)*np.sin(lam_dec));
-    z_pos=(R+h_v)*np.sin(psi_dec)
+    # Now convert these to cartesian coordinates#
+    x_pos = (R + h_v) * (np.cos(psi_dec) * np.cos(lam_dec))
+    y_pos = (R + h_v) * (np.cos(psi_dec) * np.sin(lam_dec))
+    z_pos = (R + h_v) * np.sin(psi_dec)
 
-    #now multiply by the rotation vector#
-    rot_vec=[[np.cos(2*np.pi*t_v/(sid_day)),-np.sin(2*np.pi*t_v/(sid_day)),0],[np.sin(2*np.pi*t_v/(sid_day)),np.cos(2*np.pi*t_v/(sid_day)),0],[0,0,1]];
-    v_pos_vec=np.matmul(rot_vec,[[x_pos],[y_pos],[z_pos]]);
+    # now multiply by the rotation vector#
+    rot_vec = [[np.cos(2 * np.pi * t_v / (sid_day)), -np.sin(2 * np.pi * t_v / (sid_day)), 0],
+               [np.sin(2 * np.pi * t_v / (sid_day)), np.cos(2 * np.pi * t_v / (sid_day)), 0], [0, 0, 1]];
+    v_pos_vec = np.matmul(rot_vec, [[x_pos], [y_pos], [z_pos]]);
 
-    #define the sat data
-    sat_step_data=np.zeros((24,5))
+    # define the sat data
+    sat_step_data = np.zeros((24, 5))
     for kk in range(24):
-        u=init_sat_dat[kk,0:3]
-        v=init_sat_dat[kk,3:6]
-        p=init_sat_dat[kk,6]
-        h_s=init_sat_dat[kk,7]
-        theta_s=init_sat_dat[kk,8]
+        u = init_sat_dat[kk, 0:3]
+        v = init_sat_dat[kk, 3:6]
+        p = init_sat_dat[kk, 6]
+        h_s = init_sat_dat[kk, 7]
+        theta_s = init_sat_dat[kk, 8]
 
-        #the initial time value is just the time at the car#
-        error =1;
-        t_old=t_v;
-        while error > 3*10**-(17):
-            #new_t=t_v-(np.linalg.norm(v_pos_vec-sat_pos(u,v,p,t_old,R,h_s,theta_s,dat_pi),2))/c
-            iterator_sat_pos=sat_pos(u,v,p,t_old,R,h_s,theta_s,dat_pi);
-            new_t=t_v-(np.sqrt((iterator_sat_pos[0]-v_pos_vec[0])**2+(iterator_sat_pos[1]-v_pos_vec[1])**2+(iterator_sat_pos[2]-v_pos_vec[2])**2))/c
-            error=abs(t_old-new_t)
-            t_old=new_t;
+        # the initial time value is just the time at the car#
+        error = 1;
+        t_old = t_v;
+        while error > 3 * 10 ** -(17):
+            # new_t=t_v-(np.linalg.norm(v_pos_vec-sat_pos(u,v,p,t_old,R,h_s,theta_s,dat_pi),2))/c
+            iterator_sat_pos = sat_pos(u, v, p, t_old, R, h_s, theta_s, dat_pi);
+            new_t = t_v - (np.sqrt(
+                (iterator_sat_pos[0] - v_pos_vec[0]) ** 2 + (iterator_sat_pos[1] - v_pos_vec[1]) ** 2 + (
+                            iterator_sat_pos[2] - v_pos_vec[2]) ** 2)) / c
+            error = abs(t_old - new_t)
+            t_old = new_t;
 
-        sat_signal_pos=sat_pos(u,v,p,t_old,R,h_s,theta_s,dat_pi)
-        sat_step_data[kk,0]=kk
-        sat_step_data[kk,1]=t_old
-        sat_step_data[kk,2]=sat_signal_pos[0]
-        sat_step_data[kk,3]=sat_signal_pos[1]
-        sat_step_data[kk,4]=sat_signal_pos[2]
-        #now check to see if those dudes are above the horizon#
-        pos_vec_dot=(sat_signal_pos[0]*v_pos_vec[0]+sat_signal_pos[1]*v_pos_vec[1]+sat_signal_pos[2]*v_pos_vec[2])
-        v_mag_sqd=np.linalg.norm(v_pos_vec)**2
-        #now check to see if those dudes are above the horizon#
-        if pos_vec_dot>v_mag_sqd:
-            to_be_output.append(sat_step_data[kk,:])#if they are, append them to the array
+        sat_signal_pos = sat_pos(u, v, p, t_old, R, h_s, theta_s, dat_pi)
+        sat_step_data[kk, 0] = kk
+        sat_step_data[kk, 1] = t_old
+        sat_step_data[kk, 2] = sat_signal_pos[0]
+        sat_step_data[kk, 3] = sat_signal_pos[1]
+        sat_step_data[kk, 4] = sat_signal_pos[2]
+        # now check to see if those dudes are above the horizon#
+        pos_vec_dot = (
+                    sat_signal_pos[0] * v_pos_vec[0] + sat_signal_pos[1] * v_pos_vec[1] + sat_signal_pos[2] * v_pos_vec[
+                2])
+        v_mag_sqd = np.linalg.norm(v_pos_vec) ** 2
+        # now check to see if those dudes are above the horizon#
+        if pos_vec_dot > v_mag_sqd:
+            to_be_output.append(sat_step_data[kk, :])  # if they are, append them to the array
 
-output_array=np.squeeze(to_be_output)
+output_array = np.squeeze(to_be_output)
 for point in output_array:
     sys.stdout.write('{:0.0f} {:6.16e} {:6.16e} {:6.16e} {:6.16e}\n'.format(point[0],
                                                                             point[1], point[2], point[3], point[4]))
