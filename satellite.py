e6bcc54134cbebd2963565ea8b5b43cc8551f21a
Lukas Gust <lukasgust31@gmail.com>
2018-11-11 15:15:58 -0700

    satellite logging finished

--- satellite.py
+++ satellite.py
@@ -54,6 +54,7 @@ sid_day = np.nan
 with open('./data.dat', 'r') as initial_loc:
     logger.write('\n\n--- Reading data.dat ---\n\n')
     for i, line in enumerate(initial_loc.readlines()):
+        logger.flush()
         current_line = np.float64(line[1:26])
         description = line.split('/=')[1].strip()
         if i == 0:
@@ -75,7 +76,7 @@ with open('./data.dat', 'r') as initial_loc:
 
 logger.write('\n\n--- End of data.dat ---\n\n')
 
-vehicle_input_str = sys.stdin.read().rstrip()
+vehicle_input_str = sys.stdin.read().strip()
 pipe_input = np.array(vehicle_input_str.split('\n'))
 if pipe_input.shape[0] == 0:  # if there is no input throw an error
     raise IOError('No satellite input provided. Pipe in input from file or other executable in standard UNIX fashion')
@@ -90,7 +91,12 @@ for i, line in enumerate(pipe_input):
 to_be_output = []  # this will have the data from the satellites who are above the horizon appended at each step
 
 for ns in range(num_steps):
+    logger.flush()
+    logger.write('\nStep: ' + str(ns) + '\n')
     prelim_path_data = path_data[ns, :]
+    logger.write('Read: {:0.16} {:0.16} {:0.16} {:0.16} {:0.16} {:0.16} {:0.16} {:0.16} {:0.16} {:0.16}\n'.format(
+        prelim_path_data[0], prelim_path_data[1], prelim_path_data[2], prelim_path_data[3], prelim_path_data[4],
+        prelim_path_data[5], prelim_path_data[6], prelim_path_data[7], prelim_path_data[8], prelim_path_data[9]))
     # latitude angles
     deg_lat = prelim_path_data[1]
     min_lat = prelim_path_data[2]
@@ -121,6 +127,7 @@ for ns in range(num_steps):
 
     # define the sat data
     sat_step_data = np.zeros((24, 5))
+    logger.write('Wrote:\n')
     for kk in range(24):
         u = init_sat_dat[kk, 0:3]
         v = init_sat_dat[kk, 3:6]
@@ -151,12 +158,20 @@ for ns in range(num_steps):
         v_mag_sqd = np.linalg.norm(v_pos_vec) ** 2
         # now check to see if those dudes are above the horizon#
         if pos_vec_dot > v_mag_sqd:
+            logger.write('{:0.0f} {:6.16e} {:6.16e} {:6.16e} {:6.16e}\n'.format(sat_step_data[kk, 0],
+                                                                                sat_step_data[kk, 1],
+                                                                                sat_step_data[kk, 2],
+                                                                                sat_step_data[kk, 3],
+                                                                                sat_step_data[kk, 4]))
             to_be_output.append(sat_step_data[kk, :])  # if they are, append them to the array
 
 output_array = np.squeeze(to_be_output)
 for point in output_array:
     sys.stdout.write('{:0.0f} {:6.16e} {:6.16e} {:6.16e} {:6.16e}\n'.format(point[0],
-                                                                            point[1], point[2], point[3], point[4]))
+                                                                            point[1],
+                                                                            point[2],
+                                                                            point[3],
+                                                                            point[4]))
 
 logger.close()
 # End of program
